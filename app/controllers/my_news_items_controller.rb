20cfddc4eb2991eb7b7c49de82df5b22f176e36b
Kambiz Reza <kambizreza@berkeley.edu>
2023-12-05 00:54:44 +0000

    fixed merge conflicts

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
+require 'news-api'
+
 class MyNewsItemsController < SessionController
   before_action :set_representative
   before_action :set_representatives_list
   before_action :set_issues_list
   before_action :set_issue
   before_action :set_news_item, only: %i[edit update destroy]
-
   def new
     @news_item = NewsItem.new
   end
-
   def edit; end
 
   def create
-    @news_item = NewsItem.new(news_item_params)
+    @representatve = Representative.find(params[:representative_id])
+    @news_item = @representative.news_items.build(news_item_params)
+
     if @news_item.save
       redirect_to representative_news_item_path(@representative, @news_item),
-                  notice: 'News item was successfully created.'
+                  notice: 'News item was successfully created.'  
     else
       render :new, error: 'An error occurred when creating the news item.'
     end
@@ -36,6 +38,26 @@ class MyNewsItemsController < SessionController
     @news_item.destroy
     redirect_to representative_news_items_path(@representative),
                 notice: 'News was successfully destroyed.'
+  end
+
+  def fetch_articles
+    #Rails.logger.debug { "About to make News API call for issue: #{params[:issue]}" }
+    newsapi = News.new(Rails.application.credentials[:GOOGLE_NEWS_API_KEY])
+    result = newsapi.get_everything(
+      q:        "#{params[:issue]} AND #{@representative.name}",
+      language: 'en',
+      sortBy:   'popularity',
+      pageSize: 5
+    )
+    #Rails.logger.debug { "News API response: #{result.inspect}" }
+    if result.any?
+      @articles = result
+      #Rails.logger.debug { "Articles variable set. Articles: #{@articles}" }
+      render 'my_news_items/fetch_articles'
+    else
+      #Rails.logger.debug { "No articles found for issue: #{params[:issue]}" }
+      redirect_to new_representative_my_news_item_path(@representative), alert: 'No articles found for this issue.'
+    end
   end
 
   private
@@ -70,6 +92,6 @@ class MyNewsItemsController < SessionController
 
   # Only allow a list of trusted parameters through.
   def news_item_params
-    params.require(:news_item).permit(:news, :title, :description, :link, :representative_id, :issue)
+    params.require(:news_item).permit(:title, :description, :link, :rating)
   end
 end
